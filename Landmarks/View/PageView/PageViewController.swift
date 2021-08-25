822338198dab8fa45212c882ad2883db60ce0893
Oleg Derevyanko <derevyanko@MacBook-Pro-de-Oleg.local>
2021-08-25 12:08:23 +0300

    #32. Create a View to Represent a UIPageViewController.

---
+++ Landmarks/View/PageView/PageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Oleg Derevyanko on 23.08.2021.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
